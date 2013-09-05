033990cf2266b7fdef6bb2ac0c545f965f2c1980
Vahur Roosimaa <vahur.roosimaa@gmail.com>
2013-09-05 17:55:19 +0100

    Added agent scores, Maps for agents' properties

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -5,5 +5,14 @@ class AgentsController < ApplicationController
 
   def show
     @agent = Agent.find(params[:id])
+    # Score is the percentage of available listings
+    @score = ((1 - Float(@agent.listings.where(:availability_score.gt => 0).count) / @agent.listings.count) * 100).ceil
+
+    @location = [0, 0]
+    @agent.listings.each do |listing|
+      @location[0] += listing.location[0]
+      @location[1] += listing.location[1]
+    end
+    @location = [@location[0]/@agent.listings.count, @location[1]/@agent.listings.count]
   end
 end
